3fa0dabf448fd144304ed7b460930c6fd0fcc490
Marco Lisci <info@badshark.io>
2014-10-31 14:00:36 +0100

    Remove old topic class code.

--- lib/rmsg/topic.rb
+++ lib/rmsg/topic.rb
@@ -1,25 +1,4 @@
 module Rmsg
   class Topic
-    def initialize(params)
-      @rabbit = params[:rabbit]
-      @exchange = @rabbit.channel.topic(params[:topic])
-    end
-
-    def publish(message, key)
-      @exchange.publish(message.to_json, :routing_key => key)
-    end
-
-    def subscribe(key)
-      @queue = @rabbit.channel.queue("", :exclusive => true)
-      @queue.bind(@exchange, :routing_key => key)
-      begin
-        @queue.subscribe(:block => true) do |delivery_info, metadata, payload|
-          message = JSON.parse payload
-          yield message
-        end
-      rescue Interrupt => _
-        @rabbit.close
-      end
-    end
   end
 end
