e4248de3ef6fbc24971cfb9290dbc93128a2c574
Marco Lisci <info@badshark.io>
2014-11-06 22:38:55 +0100

    Consistency of pub/sub naming.

--- test/spec/topic_spec.rb
+++ test/spec/topic_spec.rb
@@ -18,18 +18,17 @@ describe 'Topic' do
     @topic.must_be_instance_of Rmsg::Topic
   end
 
-  # Simulate two processes communicating using a topic and a key
-  it 'can publish and read back a message' do
+  it 'works in a single publisher, single subscriber scenario' do
     read_message = ''
-    consumer = 'bundle exec ruby test/bin/topic_consumer.rb'
+    subscriber = 'bundle exec ruby test/bin/topic_subscriber.rb'
     publisher = 'bundle exec ruby test/bin/topic_publisher.rb'
 
-    Open3.popen3(consumer) do |stdin, stdout, stderr, wait_thr|
-      # Wait for the consumer to be up
+    Open3.popen3(subscriber) do |stdin, stdout, stderr, wait_thr|
+      # Wait for the subscriber to be up
       sleep 2
       spawn(publisher)
       read_message = stdout.gets
-      # Kill long-running consumer
+      # Kill long-running subscriber
       Process.kill('INT', wait_thr.pid)
     end
 
